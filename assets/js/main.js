04201fe802dad1856de9b2eb1dee154fc9dc81e3
sobuj-hasan <sobujhasan388@gmail.com>
2022-01-03 11:06:02 +0600

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,13 @@
+$('.banner-autoplay').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    dots: true,
+    prevArrow: '#prevarrows',
+    nextArrow: '#nextarrows',
+    fade: true,
+    autoplaySpeed: 2000,
+});
+
+
+
